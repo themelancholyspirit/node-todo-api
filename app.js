9bcdadab904828f44471b25a490d8dfd2102a87b
Guga <Masterrrguga@gmail.com>
2023-01-09 17:00:26 +0400

    Visualize routes

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ const app = express();
 
 require("dotenv").config();
 
-console.log(process.env.PORT);
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("hello"));
+app.get("/tasks", (req, res) => {
+  res.status(200).send({ tasks: [] });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  res.status(200).send({ task: "single task" });
+});
+
+app.post("/tasks", (req, res) => {
+  res.status(200).send({ msg: "task added successfully" });
+});
+
+app.put("/tasks/:id", (req, res) => {
+  res.status(200).send({ msg: "task updated successfully" });
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  res.status(200).send({ msg: "task deleted successfully" });
+});
+
+app.listen(port, () => console.log(`server is listening on ${port}`));
